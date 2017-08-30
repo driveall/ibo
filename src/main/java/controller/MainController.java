046800c06da64619ca90d4e4ca02a592bc8be852
Ivan <ivanburlyaev@gmail.com>
2017-08-30 15:19:03 +0300

    Test commit

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,9 +1,16 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class MainController {
 
+    public ModelAndView showMain(HttpServletRequest request){
+
+        return null;
+    }
 }
