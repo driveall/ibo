7f1b8ec3d13779095d5c0809fc0b1df4eda235ce
yevheniiaV <yevheniia.vorobei@gmail.com>
2017-08-30 16:54:14 +0200

    fix 404

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,17 +1,19 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.RequestWrapper;
 
 
 @Controller
 public class MainController {
-
-
+    @RequestMapping(value = {"/", "main.html", "index.html"})
     public ModelAndView showMain(HttpServletRequest request){
-
-        return null;
+        ModelAndView main = new ModelAndView("main");
+        return main;
     }
 }
